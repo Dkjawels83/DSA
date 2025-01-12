8b465513f34986adfb55e334902ec3c17e1495d1
Dkjawels83 <150531987+Dkjawels83@users.noreply.github.com>
2025-01-12 15:11:08 +0530

    Create CountPrime.cpp

---
+++ math For DSA/CountPrime.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    int countPrimes(int n) {
+        if (n <= 2) return 0; 
+
+        vector<bool> isPrime(n, true); 
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+
+                for (int j = i * 2; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return count; 
+    }
+
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int result = countPrimes(n);
+    cout << "Number of prime numbers less than " << n << " is: " << result << endl;
+
+    return 0;
+}
+
