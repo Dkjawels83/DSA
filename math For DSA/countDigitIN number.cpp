615b2a5fcf007358ef4bc2de612806ae1dac01a2
Dkjawels83 <150531987+Dkjawels83@users.noreply.github.com>
2025-01-12 15:16:06 +0530

    Create countDigitIN number.cpp

---
+++ math For DSA/countDigitIN number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void printDigit(int n)
+{
+	while(n!=0)
+	{
+		int digit=n%10;
+		cout<<digit<<endl;
+		n=n/10;
+	}
+}
+
+
+int main()
+{
+	int n;
+cout<<"enter the number"<<endl;
+cin>>n;
+printDigit(n);
+return 0;
+	
+}
