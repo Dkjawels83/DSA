cca8fc9aa99067872ef5526f9857703deb7731f5
Dkjawels83 <150531987+Dkjawels83@users.noreply.github.com>
2025-01-28 23:27:25 +0530

    Create lucky number.cpp

---
+++ math For DSA/lucky number.cpp
@@ -0,0 +1,35 @@
+// C++ program for Lucky Numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isLucky(int n)
+{
+    static int counter = 2;
+
+    if (counter > n)
+        return 1;
+    if (n % counter == 0)
+        return 0;
+
+  
+    int next_position = n - (n / counter);
+
+    counter++;
+    return isLucky(next_position);
+}
+
+// Driver Code
+int main()
+{
+    int x = 7;
+
+    // Function call
+    if (isLucky(x))
+        cout << x << " is a lucky no.";
+    else
+        cout << x << " is not a lucky no.";
+}
+
+
